afa845e8fc323124abb0cfe75bf3373554c8f139
lmenon <lavanyamenon1215@gmail.com>
2024-02-03 01:08:26 -0800

    added functionality to log in form and created structure for db.json

--- app/src/components/LogInForm.js
+++ app/src/components/LogInForm.js
@@ -1,20 +1,31 @@
 import React from 'react'
 
-const LogInForm = () => {
+function LogInForm () {
+
+  const logIn = (e) => {
+    e.preventDefault();
+
+    const rawFormData = new FormData(e.target)
+    const formData = Object.fromEntries(rawFormData)
+
+    console.log(formData);
+  }
+
+
   return (
     <div className='logInForm'>
       <h1>Log In</h1>
-      <form id='logInForm'>
+      <form id='logInForm' onSubmit={logIn}>
         <div className='label-div'>
           <label htmlFor='username'>Username</label>
           <label htmlFor='password'>Password</label>
         </div>
         <div className='input-div'>
-          <input type='text' id='username' name='username'/>
-          <input type='password' id='password' name='password'/>
-        </div>        
+          <input type='text' id='username' name='username'placeholder='Username' />
+          <input type='password' id='password' name='password' placeholder='Password'/>
+        </div> 
+        <button type='submit' id='logInButton'>Log In</button>       
       </form>
-      <button type='submit' id='logInButton'>Log In</button>
     </div>
   )
 }
