23c9f003723ee9344697ed59fa2e704f20735d77
lmenon <lavanyamenon1215@gmail.com>
2024-02-03 18:11:58 -0800

    got map markers dynamically loading brand data

--- app/src/components/MapComponent.js
+++ app/src/components/MapComponent.js
@@ -1,19 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import {MapContainer, TileLayer, Marker} from "react-leaflet"
 import "leaflet/dist/leaflet.css"
+import { Icon } from 'leaflet'
 
-const MapComponent = async() => {
+const MapComponent = () => {
+  const [brandsJSON, setBrandsJSON] = useState([]);
 
-  const response = await fetch('http://localhost:3030/Brands');
-  const brands = await response.json();
-  const brandsJSON = []
-
-  brands.forEach((brand) => {
-    brandsJSON.push(brand.address)
+  const markerIcon = new Icon({
+    // <a href="https://www.flaticon.com/free-icons/marker" title="marker icons">Marker icons created by kmg design - Flaticon</a>
+    iconUrl: "https://drive.google.com/thumbnail?id=1It7S0dGUP20xOrdPoFIO--nRPi4BxqeE", 
+    iconSize: [40,40]
   })
 
-  console.log(brands);
-  console.log(brandsJSON);
+  // load all of the coordinates of all the brands from the JSON server 
+  const loadMarkers = async () => {
+
+    try {
+      const response = await fetch('http://localhost:3030/Brands');
+      const brands = await response.json();
+
+      const addresses = brands.map((brand) => [brand.address.lat, brand.address.lon]);
+      setBrandsJSON(addresses);
+      // console.log(brandsJSON);
+    } catch (error) {
+      console.error('Error loading markers:', error);
+    }
+  }
+
+ 
+
+  useEffect(() => {
+    loadMarkers()
+  }, [])
 
 
   return (
@@ -21,6 +39,14 @@ const MapComponent = async() => {
           <TileLayer
               url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
           />
+          {
+            brandsJSON.map((position, index) => (
+              <Marker key={index} position={position} icon={markerIcon}>
+              </Marker>
+            ))
+          }
+          {/* <Marker position={[40.7128, -74.0060]} icon={markerIcon}>
+          </Marker> */}
       </MapContainer>
 
       
